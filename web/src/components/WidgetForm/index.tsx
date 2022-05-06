7879d38fc117c7265275e13291c24f403f7e7ed5
Pedro Paulo <pedropaulotj@gmail.com>
2022-05-05 22:25:50 -0300

    feat: Stage 2 terminado

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,82 @@
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de um balão de pensamento",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbacksent, setFeedbackSent] = useState(false);
+
+  function handleStartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbacksent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleStartFeedback}/>
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleStartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela{" "}
+        <a
+          href="https://www.rocketseat.com.br/"
+          className="underline underline-offset-2"
+          target="_blank"
+        >
+          Rocketseat
+        </a>{" "}
+        ft.{" "}
+        <a
+          href="https://pedropaulo.dev"
+          className="underline underline-offset-2"
+          target="_blank"
+        >
+          Pedro Paulo
+        </a>
+      </footer>
+    </div>
+  );
+}
